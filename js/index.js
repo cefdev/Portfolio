80e736f567b29c0c454b80a91282965eb5f9c9a2
Moncef <moncef.koumila@gmail.com>
2021-03-17 19:57:34 +0000

    Make the Mobile navigation responsive

--- js/index.js
+++ js/index.js
@@ -1,13 +1,30 @@
 const hamburger = document.querySelector(".hamburger");
+const mobileMenu = document.querySelector(".mobile");
+const mobileLinks = document.querySelectorAll(".mobile_links a");
 
 let menuOpen = false;
 
 hamburger.addEventListener("click", () => {
   if (!menuOpen) {
     hamburger.classList.add("open");
+    // Show the Mobile Menu
+    mobileMenu.style.transform = "translateX(0)";
     menuOpen = true;
   } else {
     hamburger.classList.remove("open");
+    // Hide the Mobile Menu
+    mobileMenu.style.transform = "translateX(100%)";
     menuOpen = false;
   }
+});
+
+// Hide the Mobile Menu if user clicked on menu links
+mobileLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    // Hide the Mobile Menu
+    mobileMenu.style.transform = "translateX(100%)";
+    // Turn the Hmaburger to the initial form
+    hamburger.classList.remove("open");
+    menuOpen = false;
+  });
 });
